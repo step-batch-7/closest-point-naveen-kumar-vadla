feab673d8ed178705a26f2e89bbf6581f0be0c6f
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-04-13 10:36:39 +0530

    added organism_locations array of Points

--- main.c
+++ main.c
@@ -4,5 +4,6 @@
 int main(void)
 {
   Point food[] = {{18, 76}, {19, 66}, {89, 57}, {9, 71}, {55, 38}};
+  Point organism_locations[] = {{18, 86}, {97, 27}, {69, 7}, {10, 94}, {36, 27}};
   return 0;
 }
