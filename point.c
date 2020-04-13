d8deac9eb6a59385c9bb0a9d082c39da7068671a
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-04-13 11:32:37 +0530

    implemented get_closest_food which takes food_points ,closest_food, length and current_location then modifies closest_food to the nearest food_point

--- point.c
+++ point.c
@@ -13,4 +13,15 @@ void get_closest_food(Point food_points[], int points_length, Point current_loca
 {
   closest_food_location->x = current_location.x;
   closest_food_location->y = current_location.y;
+  double min_distance = MAX_DISTANCE;
+  FOR_LOOP(0, points_length)
+  {
+    double distance_to_food = distance_between_two_points(current_location, food_points[i]);
+    if (distance_to_food < min_distance)
+    {
+      min_distance = distance_to_food;
+      closest_food_location->x = food_points[i].x;
+      closest_food_location->y = food_points[i].y;
+    }
+  }
 }
